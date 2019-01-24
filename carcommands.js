b3c5c7991a946b3ac744ab54c1fddc6eb911517a
Mojan Jamalzadeh <mojanjamalzadeh@yahoo.com>
2019-01-24 11:33:50 -0800

    new baud rate

--- carcommands.js
+++ carcommands.js
@@ -4,7 +4,7 @@ var SerialPort = require('/home/root/node-serialport');
 const ActualBinding = SerialPort.Binding;
 ActualBinding.createPort('/dev/ttyUSB0',{ echo: true, record: true });
 const port = new SerialPort('/dev/ttyUSB0', {
-  baudRate: 19200
+  baudRate: 115200
 });
 
 port.on('open', function() {
